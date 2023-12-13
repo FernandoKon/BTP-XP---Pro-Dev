3a1e98d6d2ea91ff05e641fca834bdbc4789d599
fernando <fehkon@hotmail.com>
2023-12-12 21:10:24 -0300

    fix and refactor

--- webapp/controller/UserProfile.controller.js
+++ webapp/controller/UserProfile.controller.js
@@ -1,63 +1,83 @@
 sap.ui.define(
-    [
-      "sap/ui/core/mvc/Controller",
-      "sap/ui/model/json/JSONModel",
-      "com/lab2dev/btpxp/model/formatter",
-    ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (
-      Controller,
-      JSONModel,
-      formatter,
-    ) {
-      "use strict";
-  
-      return Controller.extend("com.lab2dev.btpxp.controller.UserProfile", {
-        formatter: formatter,
-  
-        onInit: function () {
-            this.oRouter = this.getOwnerComponent().getRouter();
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "com/lab2dev/btpxp/model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+    "use strict";
 
-            this.oRouter
-            .getRoute("UserProfile")
-            .attachPatternMatched(this.onRouteMatch, this);
+    return Controller.extend("com.lab2dev.btpxp.controller.UserProfile", {
+      formatter: formatter,
 
-            
-        },
-  
-        onRouteMatch: function (oEvent) {
-          const oArguments = oEvent.getParameter("arguments")
-          
-          this.getUserInfo(oArguments.Name).then((e) => {
-            const oModel = new JSONModel(e);
-            this.getView().setModel(oModel, "userProfile");
-          })
-        },
+      onInit: function () {
+        this.oRouter = this.getOwnerComponent().getRouter();
 
-        getUserInfo: async function (name) {
-          const oModel = new JSONModel();
-          await oModel.loadData("/model/ranking.json");
-          const oData = oModel.getData()
-          console.log(oData)
-        
-        return oData.find((el) => el.name === String(name))
+        this.oRouter
+          .getRoute("UserProfile")
+          .attachPatternMatched(this.onRouteMatch, this);
       },
-        
-        onOpenDialog: function () {  
-          if (!this.dialog) {
-            this.dialog = sap.ui.xmlfragment(
-              "com.lab2dev.btpxp.view.fragments.Dialog",
-              this
-            );
-            this.getView().addDependent(this.dialog);
-          }
-  
-          this.dialog.open();
-        },
 
-      });
-    }
-  );
-  
+      onRouteMatch: function (oEvent) {
+        const oArguments = oEvent.getParameter("arguments");
+
+        this.getUserInfo(oArguments.Name).then((e) => {
+          const oModel = new JSONModel(e);
+          this.getView().setModel(oModel, "userProfile");
+        });
+      },
+
+      getUserInfo: async function (name) {
+        const oModel = new JSONModel();
+        await oModel.loadData("/model/ranking.json");
+        const oData = oModel.getData();
+
+        oData.sort((a, b) => b.score - a.score);
+
+        oData.forEach((item, index) => (item.position = index + 1));
+
+        return oData.find((el) => el.name === String(name));
+      },
+
+      onOpenDialog: function () {
+        if (!this.dialog) {
+          this.dialog = sap.ui.xmlfragment(
+            "com.lab2dev.btpxp.view.fragments.UserDialog",
+            this
+          );
+          this.getView().addDependent(this.dialog);
+        }
+
+        this.dialog.open();
+      },
+
+      onSearch: function (oEvent) {
+        const aFilters = [];
+        const sQuery = oEvent.getSource().getValue();
+        if (sQuery && sQuery.length > 0) {
+          const filterTasks = new Filter({
+            filters: [
+              new Filter("name", FilterOperator.Contains, sQuery),
+              new Filter("ID", FilterOperator.EQ, sQuery),
+            ],
+          });
+          aFilters.push(filterTasks);
+        }
+
+        const oTable = this.byId("idTaskTable");
+        const oBinding = oTable.getBinding("items");
+        oBinding.filter(aFilters);
+      },
+
+      onCloseDialog: function () {
+        this.dialog.close()
+      },
+
+    });
+  }
+);
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,9 +1,25 @@
 sap.ui.define([
-    
- ], function () {
+   "sap/ui/core/format/DateFormat"
+ ], function (DateFormat) {
     "use strict";
  
     return {
-        
+      formatDate: function (oDate) {
+         if (!oDate) {
+             return "";
+         }
+     
+         const oDateFormat = DateFormat.getDateTimeInstance({
+             style: "medium",
+             pattern: "dd/MM/yyyy' - 'HH'h'mm"
+         });
+
+         // Certifique-se de que oDate é um objeto Date
+         if (!(oDate instanceof Date)) {
+            oDate = new Date(oDate);
+         }
+
+         return oDateFormat.format(oDate);
+     },
     };
  });
